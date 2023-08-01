17eebdcb87e35c98c627bf0256790a02f4a8cd70
Omkar Marwadi <71593426+Omkar-coderbot@users.noreply.github.com>
2023-08-01 21:52:58 +0530

    Update environment.ts

--- src/app/envirorment/environment.ts
+++ src/app/envirorment/environment.ts
@@ -1,6 +1,9 @@
 // environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:7115/api/', // Replace with your development API URL
+    // apiUrl: 'http://localhost:7115/api/', // Replace with your development API URL
+     apiUrl: 'http://52.66.152.140:7124/api/'
+    
+  
   };
   
