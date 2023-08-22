0dff6c5b9fe54d539bb4733179d8843688b80afe
Omkar Marwadi <71593426+Omkar-coderbot@users.noreply.github.com>
2023-08-22 20:40:00 +0530

    Update product.component.ts

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -49,7 +49,7 @@ refreshList(){
     // Save user data using the UserService
     
     this.productService.saveProduct(this.product).subscribe(res=>{
-      alert("User Added Successfully ! ");
+      alert("Product Added Successfully ! ");
       console.log(this.product);
        this.refreshList();
    
